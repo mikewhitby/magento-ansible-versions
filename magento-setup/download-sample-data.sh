2e8ae867f94e67d7af69d7e02c2c62ad19f67171
Mike Whitby <m.whitby@gmail.com>
2015-03-23 14:31:34 +0000

    Fix download location

--- magento-setup/download-sample-data.sh
+++ magento-setup/download-sample-data.sh
@@ -8,17 +8,17 @@ parentdir="$(dirname $(readlink -f $0))"
 
 if [ ! -d "$parentdir/sample-data/magento-sample-data-1.9.1.0" ]; then
 	echo "Downloading sample data for 1.9.1.0"
-	curl -s https://raw.githubusercontent.com/Vinai/compressed-magento-sample-data/1.9.1.0/compressed-magento-sample-data-1.9.1.0.tgz | tar -xz -C sample-data
+	curl -s https://raw.githubusercontent.com/Vinai/compressed-magento-sample-data/1.9.1.0/compressed-magento-sample-data-1.9.1.0.tgz | tar -xz -C $parentdir/sample-data
 fi
 if [ ! -d "$parentdir/sample-data/magento-sample-data-1.9.0.0" ]; then
 	echo "Downloading sample data for 1.9.0.0"
-	curl -s https://raw.githubusercontent.com/Vinai/compressed-magento-sample-data/1.9.0.0/compressed-magento-sample-data-1.9.0.0.tgz | tar -xz -C sample-data
+	curl -s https://raw.githubusercontent.com/Vinai/compressed-magento-sample-data/1.9.0.0/compressed-magento-sample-data-1.9.0.0.tgz | tar -xz -C $parentdir/sample-data
 fi
 if [ ! -d "$parentdir/sample-data/magento-sample-data-1.6.1.0" ]; then
 	echo "Downloading sample data for 1.6.1.0"
-	curl -s http://www.magentocommerce.com/downloads/assets/1.6.1.0/magento-sample-data-1.6.1.0.tar.gz | tar -xz -C sample-data
+	curl -s http://www.magentocommerce.com/downloads/assets/1.6.1.0/magento-sample-data-1.6.1.0.tar.gz | tar -xz -C $parentdir/sample-data
 fi
 if [ ! -d "$parentdir/sample-data/magento-sample-data-1.1.2" ]; then
 	echo "Downloading sample data for 1.1.2"
-	curl -s http://www.magentocommerce.com/downloads/assets/1.1.2/magento-sample-data-1.1.2.tar.gz | tar -xz -C sample-data
+	curl -s http://www.magentocommerce.com/downloads/assets/1.1.2/magento-sample-data-1.1.2.tar.gz | tar -xz -C $parentdir/sample-data
 fi
